eed8580aacd08e07ef9edd4edc9e5f31090b72e3
Mikel Hamer <mikelhamer@gmail.com>
2024-07-10 16:52:32 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ion8-alert-bug',
+  webDir: 'www'
+};
+
+export default config;
